e941a5a9ac6f2d6fd718e205688b7075602be225
oscar islas <oscar.islas@uabc.edu.mx>
2020-04-25 22:23:55 -0700

    update

--- src/main.js
+++ src/main.js
@@ -3,10 +3,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Traer la librería
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Darlo de alta
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  methods: {
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import oauth from './modules/oauth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  // Aquuí registramos todos los módulos
   modules: {
+    oauth
   }
 })
