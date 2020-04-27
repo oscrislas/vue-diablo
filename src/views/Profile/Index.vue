a31b97d0ad3b6010d478dcb581b8cad318db6cec
oscar islas <oscar.islas@uabc.edu.mx>
2020-04-26 17:08:27 -0700

    update

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,11 +1,62 @@
 <template>
   <div>
-    <h1>Profile View</h1>
+    <BaseLoading v-if="isLoading"/>
+    <template v-if="profileData !== null">
+      <MainBlock :profileData="profileData"/>
+    </template>
   </div>
 </template>
 
 <script>
+import BaseLoading from '@/components/BaseLoading'
+import setError from '@/mixins/setError'
+import { getApiAccount } from '@/api/search'
+import MainBlock from './MainBlock/Index'
+
 export default {
-  name: 'ProfileView'
+  name: 'ProfileView',
+  mixins: [
+    setError
+  ],
+  components: { BaseLoading, MainBlock },
+  data () {
+    return {
+      isLoading: false,
+      profileData: null
+    }
+  },
+  created () {
+    this.isLoading = true
+    const { region, battleTag: account } = this.$route.params
+    this.fetchData(region, account)
+  },
+  methods: {
+    /**
+     * Obtener los datos de la API
+     * Guardarlos en 'profileData'
+     */
+    fetchData (region, account) {
+      getApiAccount({ region, account })
+        .then(({ data }) => {
+          this.profileData = data
+        })
+        .catch((err) => {
+          this.profileData = null
+          const errObj = {
+            routeParams: this.$route.params,
+            message: err.message
+          }
+          if (err.response) {
+            errObj.data = err.response.data
+            errObj.status = err.response.status
+          }
+          this.setApiErr(errObj)
+          this.$router.push({ name: 'Error' })
+        })
+        .finally(() => {
+          this.isLoading = false
+        })
+    }
+  }
 }
 </script>
